38b5cc6a276ebced54b5b0d6ec0852d74f91c7f8
underd0g1 <hide4@comcast.net>
2020-09-15 00:49:56 -0400

    added base

---
+++ script/script.js
@@ -0,0 +1,72 @@
+const Config = {
+    name: "user",
+    scale: 1,
+    Links: [
+        [
+            "site",
+            [
+                ["link", "https://www.example.com"],
+                ["link", "https://www.example.com"]
+            ]
+        ],
+        [
+            "site",
+            [
+                ["link", "https://www.example.com"],
+                ["link", "https://www.example.com"]
+            ]
+        ],
+        [
+            "site",
+            [
+                ["link", "https://www.example.com"],
+                ["link", "https://www.example.com"],
+                ["link", "https://www.example.com"]
+            ]
+        ],
+        [
+            "site",
+            [
+                ["link", "https://www.example.com"],
+                ["link", "https://www.example.com"],
+                ["link", "https://www.example.com"],
+                ["link", "https://www.example.com"]
+            ]
+        ]
+    ]
+}
+
+const Main = (() => {
+    const list = document.getElementById("list");
+    const names = document.querySelectorAll("[data-Name]");
+    const search = document.getElementById("search");
+    const form = document.forms[0];
+
+    const init = () => {
+        list.innerHTML = Config.Links.map(([gName, Links]) => `
+            <li>
+                <h1 onclick="this.parentNode.classList.toggle('hideChildren')">${gName}</h1>
+                <ul>
+                    ${Links.map(([lName, url]) => `
+                        <li>
+                            <a href="${url}">${lName}</a>
+                        </li>`
+                    ).join("")}
+                </ul>
+            </li>` 
+        ).join("")
+        
+        names.forEach(el => {
+            el.innerText = Config.name;
+        });
+
+        document.addEventListener("keydown", e => e.key.length === 1 && search.focus());
+        search.addEventListener("keydown", () => (window.event ? event.keyCode : e.which) == 13 && form.submit());
+    };
+
+    return {
+        init,
+    };
+})();
+
+Main.init()
